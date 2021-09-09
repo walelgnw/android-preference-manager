e270b6e3b37e4f953e5eea8661dce28400f3d96e
walelgn <walelgnlinux@gmail.com>
2021-09-09 13:32:07 +0300

    Preference symplyfied

--- preference/src/main/java/com/ravenioet/preference/Preference.java
+++ preference/src/main/java/com/ravenioet/preference/Preference.java
@@ -5,20 +5,29 @@ import android.content.SharedPreferences;
 import static android.content.Context.MODE_PRIVATE;
 
 public class Preference {
-    public static Preference afrihealthCoreTool;
+    public static Preference preference;
     public Context context;
-    SharedPreferences sharedPreferences = null;
-    PrefModel model = null;
-    private Preference(Context ctx) {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    public String DEFAULT_PREF = "def_prex0";
+    public String database;
+    private Preference(Context ctx, String database_x) {
         context = ctx;
-    }
-    public static Preference getPrefMan(Context context) {
-        if (afrihealthCoreTool == null) {
-            afrihealthCoreTool = new Preference(context);
+        if(database_x == null){
+            database = DEFAULT_PREF;
+        }else {
+            database = database_x;
         }
-        return afrihealthCoreTool;
+        String prefer_db = getUserId()+database;
+        sharedPreferences = context.getSharedPreferences(prefer_db, MODE_PRIVATE);
     }
-    public String getManifestUser(){
+    public static Preference getPrefMan(Context context, String database_x) {
+        if (preference == null) {
+            preference = new Preference(context,database_x);
+        }
+        return preference;
+    }
+    public String getUserId(){
         return "pref_store_user";
         //return UserManager.getInstance(context).spec_user();
     }
@@ -27,115 +36,35 @@ public class Preference {
         //return DeviceManager.getInstance(context).get_device_id();
     }
 
-    public boolean user_pref(PrefModel preference){
-        String prefer_db = getManifestUser()+preference.getPref_name();
-        sharedPreferences = context.getSharedPreferences(prefer_db, MODE_PRIVATE);
-        model = preference;
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        switch (preference.getData_type()){
-            case 1:
-                //int
-                editor.putInt(preference.getData_name(), preference.getInt_data());
-                break;
-            case 2:
-                //string
-                editor.putString(preference.getData_name(), preference.getString_data());
-                break;
-            case 3:
-                //float
-                editor.putFloat(preference.getData_name(), preference.getFloat_data());
-                break;
-            case 4:
-                //long
-                editor.putLong(preference.getData_name(), preference.getLong_data());
-                break;
-            case 5:
-                //boolean
-                editor.putBoolean(preference.getData_name(), preference.getBoolean_data());
-                break;
-        }
-        editor.apply();
-        return editor.commit();
+    public void setBoolean(String name,boolean value){
+        editor.putBoolean(name, value);
     }
-    public boolean isUTrue(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getManifestUser()+ model.getPref_name(), MODE_PRIVATE);
-        return sharedPreferences.getBoolean(name, false);
+    public void setInt(String name,int value){
+        editor.putInt(name, value);
     }
-    public int getUInt(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getManifestUser()+ model.getPref_name(), MODE_PRIVATE);
-        return sharedPreferences.getInt(name, -1);
+    public void setUString(String name,String value){
+        editor.putString(name, value);
     }
-    public String getUString(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getManifestUser()+ model.getPref_name(), MODE_PRIVATE);
-        return sharedPreferences.getString(name, "m-n-f");
+    public void setFloat(String name,float value){
+        editor.putFloat(name, value);
     }
-    public float getUFloat(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getManifestUser()+ model.getPref_name(), MODE_PRIVATE);
-        return sharedPreferences.getFloat(name, 0);
-    }
-    public long getULong(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getManifestUser()+ model.getPref_name(), MODE_PRIVATE);
-        return sharedPreferences.getLong(name, 0);
+    public void setLong(String name,long value){
+        editor.putLong(name, value);
     }
 
-    public boolean system_pref(PrefModel preference){
-        String prefer_db = getDeviceId()+preference.getPref_name();
-        sharedPreferences = context.getSharedPreferences(prefer_db, MODE_PRIVATE);
-        model = preference;
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        switch (preference.getData_type()){
-            case 1:
-                //int
-                editor.putInt(preference.getData_name(), preference.getInt_data());
-                break;
-            case 2:
-                //string
-                editor.putString(preference.getData_name(), preference.getString_data());
-                break;
-            case 3:
-                //float
-                editor.putFloat(preference.getData_name(), preference.getFloat_data());
-                break;
-            case 4:
-                //long
-                editor.putLong(preference.getData_name(), preference.getLong_data());
-                break;
-            case 5:
-                //boolean
-                editor.putBoolean(preference.getData_name(), preference.getBoolean_data());
-                break;
-        }
-        editor.apply();
-        return editor.commit();
-    }
-    public boolean isSTrue(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getDeviceId()+database, MODE_PRIVATE);
+    public boolean getBoolean(String name){
         return sharedPreferences.getBoolean(name, false);
     }
-    public int getSInt(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getDeviceId()+database, MODE_PRIVATE);
+    public int getInt(String name){
         return sharedPreferences.getInt(name, -1);
     }
-    public String getSString(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getDeviceId()+database, MODE_PRIVATE);
+    public String getString(String name){
         return sharedPreferences.getString(name, "m-n-f");
     }
-    public float getSFloat(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getDeviceId()+database, MODE_PRIVATE);
+    public float getFloat(String name){
         return sharedPreferences.getFloat(name, 0);
     }
-    public long getSLong(String database,String name){
-        sharedPreferences = context.
-                getSharedPreferences(getDeviceId()+database, MODE_PRIVATE);
+    public long getLong(String name){
         return sharedPreferences.getLong(name, 0);
     }
 
